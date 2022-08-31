7422365f6f25c83d934be49a4682ac1e001756c0
Adeniji Opeyemi <adenijiopeyemi68@gmail.com>
2022-08-31 14:54:53 +0100

    made changes

---
+++ vpc.tf
@@ -0,0 +1,87 @@
+resource "aws_vpc" "my-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+# public subnets
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" # This is what makes it a public subnet
+  availability_zone       = var.availability_zone
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Add internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "igw"
+  }
+}
+
+# route table
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "route-table"
+  }
+}
+resource "aws_route_table_association" "subnet-associate" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route-table.id
+}
+
+# Creating security group or firewall
+resource  "aws_security_group" "efs_firewall" {
+  name          = "allow"
+  description = "Allow TLS inbound traffic"
+  vpc_id        = aws_vpc.my-vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "EFS"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "efs-sg"
+  }
+}
